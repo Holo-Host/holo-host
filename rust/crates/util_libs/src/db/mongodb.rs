e07f57dd5fd207bd0fa91ead0ca1e620d233e11c
Jetttech <ldjetton21@gmail.com>
2024-12-12 20:38:37 -0600

    resolve borrow

--- rust/crates/util_libs/src/db/mongodb.rs
+++ rust/crates/util_libs/src/db/mongodb.rs
@@ -5,7 +5,6 @@ use futures::stream::TryStreamExt;
 use mongodb::results::DeleteResult;
 use mongodb::{options::IndexOptions, Client, Collection, IndexModel};
 use serde::{Deserialize, Serialize};
-use std::borrow::Borrow;
 use std::fmt::Debug;
 
 #[derive(thiserror::Error, Debug, Clone)]
@@ -23,8 +22,8 @@ where
 {
     async fn get_one_from(&self, filter: Document) -> Result<Option<T>>;
     async fn get_many_from(&self, filter: Document) -> Result<Vec<T>>;
-    async fn insert_one_into(&self, item: T) -> Result<bson::oid::ObjectId>;
-    async fn insert_many_into(&self, items: Vec<T>) -> Result<Vec<bson::oid::ObjectId>>;
+    async fn insert_one_into(&self, item: T) -> Result<String>;
+    async fn insert_many_into(&self, items: Vec<T>) -> Result<Vec<String>>;
     async fn delete_all_from(&self) -> Result<DeleteResult>;
 }
 
@@ -86,15 +85,18 @@ where
 #[async_trait]
 impl<T> MongoDbPool<T> for MongoCollection<T>
 where
-    T: Serialize + for<'de> Deserialize<'de> + Unpin + Send + Sync + Default + IntoIndexes,
-    bson::Document: Borrow<T>,
+    T: Serialize + for<'de> Deserialize<'de> + Unpin + Send + Sync + Default + IntoIndexes + Debug,
 {
     async fn get_one_from(&self, filter: Document) -> Result<Option<T>> {
+        log::info!("get_one_from filter {:?}", filter);
+
         let item = self
             .collection
             .find_one(filter)
             .await
             .map_err(ServiceError::Database)?;
+
+        log::info!("item {:?}", item);
         Ok(item)
     }
 
@@ -104,35 +106,27 @@ where
         Ok(results)
     }
 
-    async fn insert_one_into(&self, item: T) -> Result<mongodb::bson::oid::ObjectId> {
-        let doc: Document = bson::to_document(&item).unwrap();
+    async fn insert_one_into(&self, item: T) -> Result<String> {
         let result = self
             .collection
-            .insert_one(doc)
+            .insert_one(item)
             .await
             .map_err(ServiceError::Database)?;
 
-        result.inserted_id.as_object_id().ok_or(anyhow!(
-            "MongoDB Error: Failed to get the ObjectId for inserted document."
-        ))
+        Ok(result.inserted_id.to_string())
     }
 
-    async fn insert_many_into(&self, items: Vec<T>) -> Result<Vec<mongodb::bson::oid::ObjectId>> {
-        let docs: Vec<Document> = items
-            .into_iter()
-            .map(|item| bson::to_document(&item).unwrap())
-            .collect::<Vec<_>>();
-
+    async fn insert_many_into(&self, items: Vec<T>) -> Result<Vec<String>> {
         let result = self
             .collection
-            .insert_many(docs)
+            .insert_many(items)
             .await
             .map_err(ServiceError::Database)?;
 
         let ids = result
             .inserted_ids
             .values()
-            .filter_map(|id| id.as_object_id())
+            .map(|id| id.to_string())
             .collect();
         Ok(ids)
     }
@@ -163,7 +157,7 @@ mod tests {
         let client_options = ClientOptions::parse(mongo_uri).await?;
         let client = MongoDBClient::with_options(client_options)?;
 
-        let database_name = "test_db";
+        let database_name = "holo-hosting-test";
         let collection_name = "host";
         let mut host_api =
             MongoCollection::<schemas::Host>::new(&client, &database_name, collection_name).await?;
@@ -171,36 +165,63 @@ mod tests {
         // set index
         host_api.apply_indexing().await?;
 
-        let mongodb_id = oid::ObjectId::new();
-        let host = schemas::Host {
-            _id: mongodb_id.to_string(),
-            device_id: vec!["mac_test".to_string()],
-            ip_address: "127.0.0.1".to_string(),
-            remaining_capacity: 50,
-            avg_uptime: 95,
-            avg_network_speed: 500,
-            avg_latency: 10,
-            vms: vec![],
-            assigned_workloads: "test_workload".to_string(),
-            assigned_hoster: "test_hoster".to_string(),
-        };
+        fn get_mock_host() -> schemas::Host {
+            schemas::Host {
+                _id: oid::ObjectId::new().to_string(),
+                device_id: vec!["mac_id".to_string()],
+                ip_address: "127.0.0.1".to_string(),
+                remaining_capacity: 50,
+                avg_uptime: 95,
+                avg_network_speed: 500,
+                avg_latency: 10,
+                vms: vec![],
+                assigned_workloads: "workload_id".to_string(),
+                assigned_hoster: "hoster".to_string(),
+            }
+        }
 
         // insert a document
-        host_api.insert_many_into(vec![host.clone()]).await?;
+        let host_0 = get_mock_host();
+        host_api.insert_one_into(host_0.clone()).await?;
 
-        // get the one (same) document
-        let filter = doc! { "_id": mongodb_id };
-        let fetched_host = host_api.get_one_from(filter.clone()).await?;
-
+        // get one (the same) document
+        let filter_one = doc! { "_id":  host_0._id.to_string() };
+        let fetched_host = host_api.get_one_from(filter_one.clone()).await?;
         let mongo_db_host = fetched_host.unwrap();
-        assert_eq!(mongo_db_host._id, host._id);
+        assert_eq!(mongo_db_host._id, host_0._id);
+
+        // insert many documents
+        let host_1 = get_mock_host();
+        let host_2 = get_mock_host();
+        let host_3 = get_mock_host();
+        host_api
+            .insert_many_into(vec![host_1.clone(), host_2.clone(), host_3.clone()])
+            .await?;
+
+        // get many docs
+        let ids = vec![
+            host_1._id.to_string(),
+            host_2._id.to_string(),
+            host_3._id.to_string(),
+        ];
+        let filter_many = doc! {
+            "_id": { "$in": ids }
+        };
+        let fetched_hosts = host_api.get_many_from(filter_many.clone()).await?;
+
+        assert_eq!(fetched_hosts.len(), 3);
+        let ids: Vec<String> = fetched_hosts.into_iter().map(|h| h._id).collect();
+        assert!(ids.contains(&ids[0]));
+        assert!(ids.contains(&ids[1]));
+        assert!(ids.contains(&ids[2]));
 
         // delete all documents
         let DeleteResult { deleted_count, .. } = host_api.delete_all_from().await?;
-        let fetched_host = host_api.get_one_from(filter).await?;
-
-        assert_eq!(deleted_count, 1);
+        assert_eq!(deleted_count, 4);
+        let fetched_host = host_api.get_one_from(filter_one).await?;
+        let fetched_hosts = host_api.get_many_from(filter_many).await?;
         assert!(fetched_host.is_none());
+        assert!(fetched_hosts.is_empty());
 
         Ok(())
     }
--- rust/crates/util_libs/src/db/schemas.rs
+++ rust/crates/util_libs/src/db/schemas.rs
@@ -1,9 +1,8 @@
 use super::mongodb::IntoIndexes;
 use anyhow::Result;
-use bson::{self, doc, from_document, Document};
+use bson::{self, doc, Document};
 use mongodb::options::IndexOptions;
 use serde::{Deserialize, Serialize};
-use std::borrow::Borrow;
 
 pub const DATABASE_NAME: &str = "holo-hosting";
 pub const USER_COLLECTION_NAME: &str = "user";
@@ -27,7 +26,7 @@ pub struct RoleInfo {
 
 #[derive(Serialize, Deserialize, Clone, Debug, Default)]
 pub struct User {
-    pub _id: String, // *INDEXED*, Mongodb ID
+    pub _id: String, // Mongodb ID
     pub email: String,
     pub jurisdiction: String,
     pub roles: Vec<RoleInfo>,
@@ -51,22 +50,12 @@ impl IntoIndexes for User {
         let role_index_doc = doc! { "roles.role": 1 };
         let role_index_opts = Some(
             IndexOptions::builder()
-                .unique(true)
                 .name(Some("role_index".to_string()))
                 .build(),
         );
         indices.push((role_index_doc, role_index_opts));
 
         Ok(indices)
-    }
-}
-
-impl Borrow<User> for Document {
-    fn borrow(&self) -> &User {
-        match from_document::<User>(self.clone()) {
-            Ok(u) => Box::leak(Box::new(u)), // Leak the box to return a reference
-            Err(e) => panic!("Failed to convert Document to User: {}", e), // Handle deserialization error
-        }
     }
 }
 
@@ -85,15 +74,6 @@ impl IntoIndexes for Developer {
     }
 }
 
-impl Borrow<Developer> for Document {
-    fn borrow(&self) -> &Developer {
-        match from_document::<Developer>(self.clone()) {
-            Ok(d) => Box::leak(Box::new(d)), // Leak the box to return a reference
-            Err(e) => panic!("Failed to convert Document to Developer: {}", e), // Handle deserialization error
-        }
-    }
-}
-
 // ==================== Hoster Schema ====================
 #[derive(Serialize, Deserialize, Clone, Debug, Default)]
 pub struct Hoster {
@@ -106,15 +86,6 @@ pub struct Hoster {
 impl IntoIndexes for Hoster {
     fn into_indices(&self) -> Result<Vec<(Document, Option<IndexOptions>)>> {
         Ok(vec![])
-    }
-}
-
-impl Borrow<Hoster> for Document {
-    fn borrow(&self) -> &Hoster {
-        match from_document::<Hoster>(self.clone()) {
-            Ok(h) => Box::leak(Box::new(h)), // Leak the box to return a reference
-            Err(e) => panic!("Failed to convert Document to Hoster: {}", e), // Handle deserialization error
-        }
     }
 }
 
@@ -135,7 +106,7 @@ pub struct Host {
     pub _id: String,            // Mongodb ID
     pub device_id: Vec<String>, // (eg: mac_address)
     pub ip_address: String,
-    pub remaining_capacity: u64, // *INDEXED*,
+    pub remaining_capacity: u64,
     pub avg_uptime: u64,
     pub avg_network_speed: u64,
     pub avg_latency: u64,
@@ -148,26 +119,16 @@ impl IntoIndexes for Host {
     fn into_indices(&self) -> Result<Vec<(Document, Option<IndexOptions>)>> {
         let mut indices = vec![];
 
-        //  Add Email Index
-        let capacity_index_doc = doc! { "remaining_capacity": 1 };
-        let capacity_index_opts = Some(
+        //  Add Hoster Index
+        let hoster_index_doc = doc! { "assigned_hoster": 1 };
+        let hoster_index_opts = Some(
             IndexOptions::builder()
-                .unique(true)
-                .name(Some("remaining_capacity_index".to_string()))
+                .name(Some("assigned_hoster_index".to_string()))
                 .build(),
         );
-        indices.push((capacity_index_doc, capacity_index_opts));
+        indices.push((hoster_index_doc, hoster_index_opts));
 
         Ok(indices)
-    }
-}
-
-impl Borrow<Host> for Document {
-    fn borrow(&self) -> &Host {
-        match from_document::<Host>(self.clone()) {
-            Ok(h) => Box::leak(Box::new(h)), // Leak the box to return a reference
-            Err(e) => panic!("Failed to convert Document to Host: {}", e), // Handle deserialization error
-        }
     }
 }
 
@@ -185,7 +146,7 @@ pub struct HolochainEnv {
 pub struct BaseEnv {
     pub overlay_network: Option<String>,
     pub keystore_service_address: Option<String>,
-    pub size: Option<String>,
+    pub size: Option<u64>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
@@ -218,7 +179,6 @@ impl IntoIndexes for Workload {
         let developer_index_doc = doc! { "assigned_developer": 1 };
         let developer_index_opts = Some(
             IndexOptions::builder()
-                .unique(true)
                 .name(Some("assigned_developer_index".to_string()))
                 .build(),
         );
@@ -237,15 +197,6 @@ impl Default for Workload {
             assigned_developer: String::new(),
             min_hosts: 0,
             assigned_hosts: Vec::new(),
-        }
-    }
-}
-
-impl Borrow<Workload> for Document {
-    fn borrow(&self) -> &Workload {
-        match from_document::<Workload>(self.clone()) {
-            Ok(w) => Box::leak(Box::new(w)), // Leak the box to return a reference
-            Err(e) => panic!("Failed to convert Document to Workload: {}", e), // Handle deserialization error
         }
     }
 }
