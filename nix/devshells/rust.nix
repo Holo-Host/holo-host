78492d1fe9eb5d1ee29069e2e99499c216269409
Stefan Junker <mail@stefanjunker.de>
2024-12-20 13:27:03 +0100

    fix(devshells/rust): correct precedence for hostPlatform check

--- nix/devshells/rust.nix
+++ nix/devshells/rust.nix
@@ -13,12 +13,14 @@ craneLib.devShell {
       flake.devShells.${system}.default
     ]
     # Inherit inputs from rust-workspace on the platforms it's available.
-    ++ (
-      pkgs.lib.lists.optionals
-        (pkgs.lib.meta.availableOn pkgs.stdenv.hostPlatform flake.packages.${system}.rust-workspace)
+    ++ (pkgs.lib.lists.optionals
+      (pkgs.lib.meta.availableOn pkgs.stdenv.hostPlatform flake.packages.${system}.rust-workspace)
+      (
         [
           flake.packages.${system}.rust-workspace
-        ] ++ (builtins.attrValues flake.packages.${system}.rust-workspace.passthru.tests)
+        ]
+        ++ (builtins.attrValues flake.packages.${system}.rust-workspace.passthru.tests)
+      )
     );
 
   # Additional dev-shell environment variables can be set directly
