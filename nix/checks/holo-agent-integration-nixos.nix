f609b569c5d7d316231699ef5e3e113120497708
Stefan Junker <mail@stefanjunker.de>
2025-01-13 16:51:12 +0100

    WIP: feat(holo-agent): add nixos module with integration test

--- nix/checks/holo-agent-integration-nixos.nix
+++ nix/checks/holo-agent-integration-nixos.nix
@@ -1,34 +1,53 @@
 {
-  pkgs,
   flake,
+  pkgs,
   ...
 }:
 
-pkgs.testers.runNixOSTest (_: {
-  name = "holo-agent-nixostest-basic";
+pkgs.testers.runNixOSTest (
+  { nodes, lib, ... }:
+  {
+    name = "host-agent-integration-nixos";
+    meta.platforms = lib.lists.intersectLists lib.platforms.linux lib.platforms.x86_64;
 
-  nodes.machine =
-    _:
+    # TODO: add a NATS server which is used to test the leaf connection
+    # nodes.hub = { };
 
-    {
-      imports = [
-        flake.nixosModules.holo-agent
-      ];
+    nodes.agent =
+      { config, ... }:
+      {
+        imports = [
+          flake.nixosModules.holo-nats-server
+          flake.nixosModules.holo-agent
+        ];
 
-      holo.agent = {
-        enable = true;
-        rust = {
-          log = "trace";
-          backtrace = "trace";
+        holo.nats-server.enable = true;
+
+        holo.agent = {
+          enable = true;
+          autoStart = false;
+          rust = {
+            log = "trace";
+            backtrace = "trace";
+          };
+
+          nats = {
+            url = "127.0.0.1:${builtins.toString config.services.nats.port}";
+            hubServerUrl = "127.0.0.1:${builtins.toString config.services.nats.settings.leafnodes.port}";
+          };
         };
       };
-    };
 
-  # takes args which are currently removed by deadnix:
-  # { nodes, ... }
-  testScript = _: ''
-    machine.start()
-    # machine.wait_for_unit("holo-agent.service")
-    machine.wait_for_unit("default.target")
-  '';
-})
+    # takes args which are currently removed by deadnix:
+    # { nodes, ... }
+    testScript = _: ''
+      agent.start()
+
+      agent.wait_for_unit("nats.service")
+
+      # TODO: fix after/require settings of the holo-agent service to make autoStart work
+      agent.succeed("systemctl start holo-agent")
+      agent.wait_for_unit("holo-agent")
+    '';
+  }
+)
--- nix/modules/nixos/holo-agent.nix
+++ nix/modules/nixos/holo-agent.nix
@@ -1,29 +1,87 @@
 # Module to configure a machine as a holo-agent.
-
 {
-  inputs,
   lib,
   config,
+  pkgs,
+  system,
+  flake,
   ...
 }:
 
 let
-  cfg = config.holo.nats-server;
+  cfg = config.holo.agent;
 in
 {
   imports = [
-    inputs.extra-container.nixosModules.default
+    # TODO: this causes an infinite recursion. we do need this to run workloads.
+    # flake.inputs.extra-container.nixosModules.default
   ];
 
-  options.holo.agent = with lib; {
-    enable = mkOption {
+  options.holo.agent = {
+    enable = lib.mkOption {
       description = "enable holo-agent";
       default = true;
+    };
+
+    autoStart = lib.mkOption {
+      default = true;
+    };
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = flake.packages.${system}.rust-workspace;
+    };
+
+    rust = {
+      log = lib.mkOption {
+        type = lib.types.str;
+        default = "debug";
+      };
+
+      backtrace = lib.mkOption {
+        type = lib.types.str;
+        default = "1";
+      };
+    };
+
+    nats = {
+      useOsNats = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+      };
+
+      url = lib.mkOption {
+        type = lib.types.str;
+      };
+      hubServerUrl = lib.mkOption {
+        type = lib.types.str;
+      };
     };
   };
 
   config = lib.mkIf cfg.enable {
-    # TODO: add holo-agent systemd service
-    # TODO: add nats client here or is it started by the agent?
+    systemd.services.holo-agent = {
+      enable = true;
+
+      requires = lib.lists.optional cfg.nats.useOsNats "nats.service";
+      after = lib.lists.optional cfg.nats.useOsNats "nats.service";
+
+      requiredBy = lib.optional cfg.autoStart "multi-user.target";
+
+      environment = {
+        RUST_LOG = cfg.rust.log;
+        RUST_BACKTRACE = cfg.rust.backtrace;
+        NATS_URL = cfg.nats.url;
+        NATS_HUB_SERVER_URL = cfg.nats.hubServerUrl;
+      };
+
+      script = builtins.toString (
+        pkgs.writeShellScript "holo-agent" ''
+          ${lib.getExe' cfg.package "host_agent"} daemonize
+        ''
+      );
+    };
+
+    # TODO: add nats server here or is it started by the agent?
   };
 }
