f79b43110493a870473672d6b65bb5095be1f595
Stefan Junker <mail@stefanjunker.de>
2025-01-14 21:32:42 +0100

    feat(nix/lib): wrap runNixOSTest with defaults
    this is required when VM tests use nixos modules that live in a
    blueprint repository like this.

--- nix/lib/default.nix
+++ nix/lib/default.nix
@@ -10,4 +10,28 @@
       );
     in
     craneLib.overrideToolchain toolchain;
+
+  runNixOSTest' =
+    { pkgs, system }:
+
+    /*
+      looks like this:
+      args: { fn body that has access to args}
+      or this:
+      { static attrs }
+    */
+    callerArg:
+
+    let
+      callerFn = pkgs.lib.toFunction callerArg;
+    in
+
+    pkgs.testers.runNixOSTest (
+      args:
+      (pkgs.lib.recursiveUpdate {
+        defaults._module.args = {
+          inherit flake inputs system;
+        };
+      } (callerFn args))
+    );
 }
