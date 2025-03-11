13003cae7791a35f03c14c8eb64d48454ac5560d
Stefan Junker <mail@stefanjunker.de>
2025-03-11 22:37:54 +0100

    feat(orchestrator): tolerate having no credentials

--- rust/clients/orchestrator/src/admin_client.rs
+++ rust/clients/orchestrator/src/admin_client.rs
@@ -12,16 +12,17 @@ const ORCHESTRATOR_ADMIN_CLIENT_INBOX_PREFIX: &str = "_ADMIN_INBOX.orchestrator"
 pub async fn run(admin_creds_path: &Option<PathBuf>, nats_url: String) -> anyhow::Result<JsClient> {
     log::info!("nats_url : {}", nats_url);
 
-    let creds = admin_creds_path
+    let credentials = admin_creds_path
         .to_owned()
-        .map(Credentials::Path)
-        .ok_or(anyhow!("Failed to locate admin credential path."))?;
+        .map(|creds| vec![Credentials::Path(creds)]);
+
+    // .ok_or(anyhow!("Failed to locate admin credential path."))?;
 
     let admin_client = JsClient::new(JsClientBuilder {
         nats_url: nats_url.clone(),
         name: ORCHESTRATOR_ADMIN_CLIENT_NAME.to_string(),
         inbox_prefix: ORCHESTRATOR_ADMIN_CLIENT_INBOX_PREFIX.to_string(),
-        credentials: Some(vec![creds.clone()]),
+        credentials,
         request_timeout: Some(Duration::from_secs(29)),
         ping_interval: Some(Duration::from_secs(10)),
         listeners: vec![with_event_listeners(get_event_listeners())],
