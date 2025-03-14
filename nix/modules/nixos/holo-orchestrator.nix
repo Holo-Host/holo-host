b5a41933af70b6ab3076ebacd41cb1a7f63244cd
Stefan Junker <mail@stefanjunker.de>
2025-03-14 01:15:27 +0100

    fixup workload commands, code cleanups, NATS jsdomain changes

--- nix/modules/nixos/holo-orchestrator.nix
+++ nix/modules/nixos/holo-orchestrator.nix
@@ -118,17 +118,6 @@ in
         pkgs.bash
       ];
 
-      # preStart = ''
-      #   init_hub_auth() {
-      #     ${cfg.hubAuthScriptPath} ${cfg.nats.listenHost} ${builtins.toString cfg.nats.listenPort} ${builtins.toString cfg.nats.sharedCredsPath} ${builtins.toString cfg.nats.localCredsPath}
-      #     # NB: In order to succeed, the following `nsc push` command
-      #     # must be run whenever the nats-server (hub) is up and running
-      #     nsc push -A
-      #   }
-      #   init_hub_auth
-      #   echo "Finshed Hub Auth Setup"
-      # '';
-
       script = ''
         ${lib.getExe' cfg.package "orchestrator"}
       '';
