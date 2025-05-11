67bf681ab4d5d2af292d392432928bdfddb778d1
Zeeshan Abid <zeeshan.abid.com@gmail.com>
2025-05-11 05:53:54 +0500

    create user was being used as public endpoint instead of protected

--- rust/holo-public-api/src/controllers/user/mod.rs
+++ rust/holo-public-api/src/controllers/user/mod.rs
@@ -2,11 +2,11 @@ use actix_web::web;
 use utoipa::OpenApi;
 mod create_user;
 
-pub fn setup_public_controllers(cfg: &mut web::ServiceConfig) {
+pub fn setup_public_controllers(_cfg: &mut web::ServiceConfig) {}
+
+pub fn setup_private_controllers(cfg: &mut web::ServiceConfig) {
     cfg.service(create_user::create_user);
 }
-
-pub fn setup_private_controllers(_cfg: &mut web::ServiceConfig) {}
 
 pub fn setup_docs() -> utoipa::openapi::OpenApi {
     let mut openapi = utoipa::openapi::OpenApi::default();
