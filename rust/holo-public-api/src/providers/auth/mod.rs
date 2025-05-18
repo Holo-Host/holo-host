dcbf36ce0b77209eaaf4b56e55b41a6ce584496e
Zeeshan Abid <zeeshan.abid.com@gmail.com>
2025-05-18 06:32:29 +0500

    resolved import warnings

--- rust/holo-public-api/src/providers/auth/mod.rs
+++ rust/holo-public-api/src/providers/auth/mod.rs
@@ -5,7 +5,7 @@ mod permissions;
 mod roles;
 mod sign_tokens;
 
-pub use common::{generate_api_key, get_apikey_from_headers, API_KEY_HEADER};
+pub use common::{generate_api_key, get_apikey_from_headers};
 pub use get_refresh_token_version::get_refresh_token_version;
 pub use hash_apikey::hash_apikey;
 pub use permissions::{get_all_accessible_owners_from_permissions, verify_all_permissions};
@@ -13,3 +13,6 @@ pub use sign_tokens::{sign_tokens, SignJwtTokenOptions};
 
 #[cfg(test)]
 mod tests;
+
+#[cfg(test)]
+pub use common::API_KEY_HEADER;
