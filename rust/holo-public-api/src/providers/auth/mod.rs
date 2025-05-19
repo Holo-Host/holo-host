af3f1393f04f1e9166498681ef9ca61e1ba0a04d
Zeeshan Abid <zeeshan.abid.com@gmail.com>
2025-05-19 20:38:41 +0500

    cargo fmt

--- rust/holo-public-api/src/providers/auth/mod.rs
+++ rust/holo-public-api/src/providers/auth/mod.rs
@@ -1,12 +1,12 @@
 mod common;
 mod get_refresh_token_version;
 mod hash_apikey;
+mod permissions;
 mod roles;
 mod sign_tokens;
-mod permissions;
 
 pub use common::{generate_api_key, get_apikey_from_headers};
 pub use get_refresh_token_version::get_refresh_token_version;
 pub use hash_apikey::hash_apikey;
-pub use sign_tokens::{sign_tokens, SignJwtTokenOptions};
 pub use permissions::{get_all_accessible_owners_from_permissions, verify_all_permissions};
+pub use sign_tokens::{sign_tokens, SignJwtTokenOptions};
