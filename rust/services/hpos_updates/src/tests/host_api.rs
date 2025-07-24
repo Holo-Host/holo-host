1599a2b60d5f569df9023a40b8bb4cddafbac712
JettTech <ldjetton21@gmail.com>
2025-07-24 17:27:23 -0500

    add test for host hpos update api

--- rust/services/hpos_updates/src/tests/host_api.rs
+++ rust/services/hpos_updates/src/tests/host_api.rs
@@ -2,16 +2,61 @@
 mod tests {
     use super::*;
     use anyhow::Result;
-    use db_utils::mongodb::api::MongoDbAPI;
-    use db_utils::schemas::DATABASE_NAME;
-    use mock_utils::host::create_test_host;
-    use mock_utils::mongodb_runner::MongodRunner;
     use mock_utils::nats_message::NatsMessage;
     use std::sync::Arc;
 
     #[tokio::test]
-    async fn test_handle_host_update() -> Result<()> {
-        // TODO: Implement Host's HPOS Update Service API test
+    async fn test_handle_host_update_good_state() -> Result<()> {
+        let api = HostUpdatesApi {};
+
+        let starting_request_info = HostUpdateInfo {
+            request_info: HostUpdateRequest {
+                channel: "nixos-unstable".to_string(),
+                device_id: "test_device_id".to_string(),
+            },
+            state: HostUpdateState::Pending,
+            context: None,
+        };
+
+        let msg_payload = serde_json::to_vec(&starting_request_info).unwrap();
+        let msg = Arc::new(NatsMessage::new("HPOS.host.update", msg_payload).into_message());
+
+        let result = api.handle_host_update_command(msg).await?;
+
+        assert!(
+            result.info.state == HostUpdateState::Completed
+                || result.info.state == HostUpdateState::Failed
+        );
+        assert_eq!(
+            result.info.request_info.device_id,
+            starting_request_info.request_info.device_id
+        );
+        assert_eq!(
+            result.info.request_info.channel,
+            starting_request_info.request_info.channel
+        );
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_handle_host_update_response_bad_state() -> Result<()> {
+        let api = HostUpdatesApi {};
+        let completed_info = HostUpdateInfo {
+            request_info: HostUpdateRequest {
+                channel: "nixos-unstable".to_string(),
+                device_id: "test_device_id".to_string(),
+            },
+            state: HostUpdateState::Completed,
+            context: Some("Update performed successfully".to_string()),
+        };
+        let msg_payload = serde_json::to_vec(&completed_info).unwrap();
+        let msg = Arc::new(NatsMessage::new("HPOS.host.status", msg_payload).into_message());
+        let result = api.handle_host_update_command(msg).await?;
+        assert_eq!(result.info.state, HostUpdateState::Completed);
+        assert_eq!(
+            result.info.context,
+            Some("Update performed successfully".to_string())
+        );
         Ok(())
     }
 }
