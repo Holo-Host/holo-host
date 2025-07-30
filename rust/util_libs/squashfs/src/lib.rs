fc292a35108dfb9585bcfcf0944befa2de927174
Zeeshan Abid <6005448+zeeshan595@users.noreply.github.com>
2025-07-30 22:47:48 +0800

    squashfs working with command line cli

--- rust/util_libs/squashfs/src/lib.rs
+++ rust/util_libs/squashfs/src/lib.rs
@@ -1,17 +1,16 @@
 use backhand::{FilesystemReader, FilesystemWriter, InnerNode, NodeHeader};
-use std::{fs, path::Path, io::Read};
+use std::{fs, io::Read, io::Write, path::Path};
 
 fn list_files_recursively(path: &Path) -> Vec<String> {
     let mut files = Vec::new();
     if let Ok(entries) = fs::read_dir(path) {
-        for entry in entries {
-            if let Ok(entry) = entry {
-                let path = entry.path();
-                if path.is_dir() {
-                    files.extend(list_files_recursively(&path));
-                } else {
-                    files.push(path.to_string_lossy().to_string());
-                }
+        for entry in entries.flatten() {
+            let path = entry.path();
+            if path.is_dir() {
+                files.push(format!("{}/", path.to_string_lossy()));
+                files.extend(list_files_recursively(&path));
+            } else {
+                files.push(path.to_string_lossy().to_string());
             }
         }
     }
@@ -27,10 +26,33 @@ pub fn create_archive(source_dir: String, output_path: String) -> Result<(), std
     for file in files {
         let reader = fs::File::open(file.clone())?;
         let file_path = file.replace(&source_dir, "");
-        writer.push_file(reader, file_path, header)?;
+        if file_path.ends_with("/") {
+            writer.push_dir(file_path, header)?;
+        } else {
+            writer.push_file(reader, file_path, header)?;
+        }
     }
     let mut output = fs::File::create(output_path)?;
     writer.write(&mut output)?;
+    Ok(())
+}
+
+/// unarchive squashfs into a folder
+pub fn unpack_archive(package_path: String, output_path: String) -> Result<(), std::io::Error> {
+    let package_files = list_files(package_path.clone())?;
+    for package_file in package_files {
+        if package_file.ends_with("/") {
+            fs::create_dir_all(Path::new(&format!("{}{}", output_path, package_file)))?;
+            continue;
+        }
+        let file_contents = read_file(package_path.clone(), package_file.clone())?;
+        let file_path = format!("{}{}", output_path, package_file);
+        let mut file = fs::File::create(file_path)?;
+        if file_contents.is_some() {
+            let file_contents = file_contents.unwrap();
+            file.write_all(&file_contents)?;
+        }
+    }
     Ok(())
 }
 
@@ -40,7 +62,11 @@ pub fn list_files(squashfs_path: String) -> Result<Vec<String>, std::io::Error> 
     let buf_reader = std::io::BufReader::new(reader);
     let reader = FilesystemReader::from_reader(buf_reader)?;
     for node in reader.files() {
-        files.push(node.fullpath.to_string_lossy().to_string());
+        if let InnerNode::File(_) = node.inner {
+            files.push(node.fullpath.to_string_lossy().to_string())
+        } else {
+            files.push(format!("{}/", node.fullpath.to_string_lossy()))
+        }
     }
     Ok(files)
 }
